bde9a20affd8e016f80aa1b369007a2db71f358e
Elena-Weber <crochetmiracles@gmail.com>
2021-03-08 21:57:03 -0500

    commit tree a3627bfad3492b8e896a0b82fb08ce7040886a4f

---
+++ lib/artist.rb
@@ -0,0 +1,47 @@
+class Artist
+    attr_accessor :name, :songs
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select{|song|song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name)
+        if self.find(name)
+            self.find(name)
+            else
+            self.create(name)
+            end
+    end
+
+    def self.find(name)
+        self.all.find { |artist| artist.name == name }
+    end
+
+    def self.create(name)
+        artist = Artist.new(name)
+        #artist.save
+        artist
+    end
+
+    def print_songs
+        puts songs.collect{|s|s.name}
+        end
+end
